337755f3afd117229c94e7114aff676d1afc98af
lushilling <lushilling@hotmail.co.uk>
2019-08-16 14:29:16 +0100

    refactoring

--- src/test/java/com/bae/service/CoreServiceImplTests.java
+++ src/test/java/com/bae/service/CoreServiceImplTests.java
@@ -3,16 +3,21 @@ package com.bae.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CoreServiceImplTests {
 	
 	@InjectMocks
-	CoreService service;
+	CoreServiceImpl service;
 	
 	@Test
 	public void contextLoads() {
-		assertThat(service).isNull();
+		assertThat(service).isNotNull();
 	}
 
 }
