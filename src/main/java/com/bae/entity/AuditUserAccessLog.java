56e19ef04e51cbaf189efc659f6884c6fbca8087
lushilling <lushilling@hotmail.co.uk>
2019-08-16 09:48:12 +0100

    generated getters, setters and constructor for AuditUserAccessLog

--- src/main/java/com/bae/entity/AuditUserAccessLog.java
+++ src/main/java/com/bae/entity/AuditUserAccessLog.java
@@ -3,9 +3,42 @@ package com.bae.entity;
 import java.util.Date;
 
 public class AuditUserAccessLog {
-	
+
 	private String username;
 	private Long id;
 	private Date timeStamp;
-	
+
+	public AuditUserAccessLog(String username, Long id, Date timeStamp) {
+		this.username = username;
+		this.id = id;
+		this.timeStamp = timeStamp;
+	}
+
+	public AuditUserAccessLog() {
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
 }
