736336bc2390b98231d5119377661d8301953c13
lushilling <lushilling@hotmail.co.uk>
2019-08-16 10:12:26 +0100

    constructors in service layer

--- src/main/java/com/bae/controller/CoreController.java
+++ src/main/java/com/bae/controller/CoreController.java
@@ -1,5 +1,28 @@
 package com.bae.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.service.CoreService;
+
+@RestController
+@RequestMapping("/core")
 public class CoreController {
 
+	@Autowired
+	private CoreService service;
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	public CoreController(CoreService service, JmsTemplate jmsTemplate) {
+		this.service = service;
+		this.jmsTemplate = jmsTemplate;
+	}
+	
+	public CoreController() {
+	}
+	
 }
