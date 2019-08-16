d8785d5370ff6090b85a58876a70c938f7da9751
lushilling <lushilling@hotmail.co.uk>
2019-08-16 09:58:30 +0100

    empty methods implmented in CoreServiceImpl

--- src/main/java/com/bae/service/CoreServiceImpl.java
+++ src/main/java/com/bae/service/CoreServiceImpl.java
@@ -1,5 +1,49 @@
 package com.bae.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CoreServiceImpl implements CoreService {
+
+	public String getSearchByName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getSearchByLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String getSearchByCarReg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLoggedInUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAuditUserAccessLogs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getSearchLogs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String sendSearchLog() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String AuditUserAccessLogs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
