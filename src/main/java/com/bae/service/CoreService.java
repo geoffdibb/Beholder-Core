4614f6e8f5948a2d3bbb238b0d2c64d15718479f
lushilling <lushilling@hotmail.co.uk>
2019-08-16 09:56:51 +0100

    methods created in CoreService

--- src/main/java/com/bae/service/CoreService.java
+++ src/main/java/com/bae/service/CoreService.java
@@ -1,5 +1,26 @@
 package com.bae.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface CoreService {
 
+	// Collection<Search> searchCollection();
+	
+	String getSearchByName();
+
+	String getSearchByLocation();
+
+	String getSearchByCarReg();
+
+	String getLoggedInUser();
+
+	String getAuditUserAccessLogs();
+	
+	String getSearchLogs();
+	
+	String sendSearchLog();
+	
+	String AuditUserAccessLogs();	
+	
 }
