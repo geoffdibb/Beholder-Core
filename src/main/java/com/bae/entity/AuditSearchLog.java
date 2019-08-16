bd09eef786b9c073342d97b5c51e0b819ed8fc8a
lushilling <lushilling@hotmail.co.uk>
2019-08-16 09:50:45 +0100

    generated getter, setters and constructor for AuditSearchLog

--- src/main/java/com/bae/entity/AuditSearchLog.java
+++ src/main/java/com/bae/entity/AuditSearchLog.java
@@ -3,10 +3,52 @@ package com.bae.entity;
 import java.util.Date;
 
 public class AuditSearchLog {
-	
+
 	private String username;
 	private Long id;
 	private Date timeStamp;
 	private String searchTerm;
+
+	public AuditSearchLog(String username, Long id, Date timeStamp, String searchTerm) {
+		this.username = username;
+		this.id = id;
+		this.timeStamp = timeStamp;
+		this.searchTerm = searchTerm;
+	}
+
+	public AuditSearchLog() {
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public String getSearchTerm() {
+		return searchTerm;
+	}
+
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
+	}
 
 }
