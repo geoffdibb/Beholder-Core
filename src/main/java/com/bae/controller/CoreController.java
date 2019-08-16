3f0b9e28e9f7c05b21fa5e77f0fed2b70a212bb2
lushilling <lushilling@hotmail.co.uk>
2019-08-16 10:24:45 +0100

    Simple @RequestMapping added

--- src/main/java/com/bae/controller/CoreController.java
+++ src/main/java/com/bae/controller/CoreController.java
@@ -2,6 +2,7 @@ package com.bae.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,22 +26,27 @@ public class CoreController {
 	public CoreController() {
 	}
 	
+	@RequestMapping
 	public String logout() {
 		return null;
 	}
 	
+	@RequestMapping
 	public String login() {
 		return null;
 	}
 	
+	@RequestMapping
 	public String getLoggedInUser() {
 		return null;
 	}
 	
+	@RequestMapping
 	public String getAuditUserAccessLogs() {
 		return null;
 	}
 	
+	@RequestMapping
 	public String getSearchLogs() {
 		return null;
 	}
