3e416451c3ce98e1b172c3197e25093c5ebc4acb
lushilling <lushilling@hotmail.co.uk>
2019-08-16 14:26:32 +0100

    service test-ish

--- src/test/java/com/bae/service/CoreServiceImplTests.java
+++ src/test/java/com/bae/service/CoreServiceImplTests.java
@@ -1,5 +1,18 @@
 package com.bae.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
 public class CoreServiceImplTests {
+	
+	@InjectMocks
+	CoreService service;
+	
+	@Test
+	public void contextLoads() {
+		assertThat(service).isNull();
+	}
 
 }
