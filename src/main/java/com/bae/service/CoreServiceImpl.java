46e34e0f835dd410d956aaa231f1237554fbb4c4
lushilling <lushilling@hotmail.co.uk>
2019-08-19 14:11:08 +0100

    removing comments

--- src/main/java/com/bae/service/CoreServiceImpl.java
+++ src/main/java/com/bae/service/CoreServiceImpl.java
@@ -6,34 +6,25 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CoreServiceImpl implements CoreService {
-	
+
 	@Autowired
 	private RestTemplate rest;
-	
+
 	public String userLogin() {
 		return rest.getForObject("http://IPAddressForUserAPI/username", String.class);
-		//This should return the message sent from UserAPI which shows if the user has logged in or not
 	}
-	
+
 	public String search(String category, String searchTerm) {
-		return rest.getForObject("http://IPAddressForSearchAPI/"+category+"/"+searchTerm, String.class);
-		//This should return results for things that match the category and searchTerm inputed
-		//The user should be able to choose a category of Name, Location or Car Registration
+		return rest.getForObject("http://IPAddressForSearchAPI/" + category + "/" + searchTerm, String.class);
 	}
-	
+
 	public String getProfile() {
 		return rest.getForObject("http://IPAddressForSearchAPI/profile", String.class);
-		//This should return a profile as an object
-		//Using this object we can select what we want to display on the front end in react application
 	}
-	
+
 	public String getAssociates(Object profile) {
-		return rest.getForObject("http://IPAddressForSearchAPI/associates/for"+profile, String.class);
-		//This should return an array of associate objects
-		//we can then list this array in the react application
+		return rest.getForObject("http://IPAddressForSearchAPI/associates/for" + profile, String.class);
 	}
-	
-	
 
 	public String getAuditRequestLog() {
 		String auditLog = rest.getForObject("http://", String.class);
@@ -55,12 +46,10 @@ public class CoreServiceImpl implements CoreService {
 		return null;
 	}
 
-
 	public String sendAuditRequestLog() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 
 	public String sendSearchLog(Object object) {
 		// TODO Auto-generated method stub
