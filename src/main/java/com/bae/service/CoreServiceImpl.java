9475983cbb6602ed123d914bb0d36bb6bb137bdd
lushilling <lushilling@hotmail.co.uk>
2019-08-19 16:46:23 +0100

    changing url

--- src/main/java/com/bae/service/CoreServiceImpl.java
+++ src/main/java/com/bae/service/CoreServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bae.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,39 +16,67 @@ public class CoreServiceImpl implements CoreService {
 	private RestTemplate rest;
 
 	private JmsTemplate jmsTemplate;
+
+	@Value("${url.user}")
+	private String userLoginURL;
+
+	@Value("${url.search}")
+	private String searchURL;
+
+	@Value("${url.searchProfile}")
+	private String searchProfileURL;
+
+	@Value("${url.searchAssociates}")
+	private String searchAssociatesURL;
+
+	@Value("${url.AuditRequest}")
+	private String AuditRequestURL;
+
+	@Value("${url.AuditUser}")
+	private String AuditUserURL;
 	
-	@Autowired 
+	@Value("${url.AuditSearch}")
+	private String AuditSearchURL;
+
+//	url.search=http://SearchAPI:8082/
+//		url.searchProfile=http://Search:8082/profile
+//		url.searchAssociates=http://Search:8082/associates/forProfile
+//		url.AuditRequest=http://AuditAPI:8081/requestLogs/getAuditRequestLog
+//		url.AuditUser=http://AuditAPI:8081/accessLogs/getAuditUserAccessLog
+//		url.AuditSearch=http://AuditAPI:8081/searchLogs/getSearchLogs
+
+	@Autowired
 	public CoreServiceImpl(RestTemplate rest, JmsTemplate jmsTemplate) {
 		this.rest = rest;
 		this.jmsTemplate = jmsTemplate;
 	}
 
 	public String userLogin() {
-		return rest.getForObject("http://UserAPI/username", String.class);
+		return rest.getForObject(userLoginURL, String.class);
 	}
 
 	public String search(String category, String searchTerm) {
-		return rest.getForObject("http://SearchAPI/" + category + "/" + searchTerm, String.class);
+		return rest.getForObject(searchURL + category + "/" + searchTerm, String.class);
 	}
 
 	public String getProfile(long id) {
-		return rest.getForObject("http://SearchAPI/profile" + id, String.class);
+		return rest.getForObject(searchProfileURL + id, String.class);
 	}
 
 	public String getAssociates(long id) {
-		return rest.getForObject("http://SearchAPI/associates/forProfile" + id, String.class);
+		return rest.getForObject(searchAssociatesURL + id, String.class);
 	}
 
 	public String getAuditRequestLog() {
-		return rest.getForObject("http://AuditAPI/requestlogs/getAuditRequestLogs", String.class);
+		return rest.getForObject(AuditRequestURL, String.class);
 	}
 
 	public String getAuditUserAccessLog() {
-		return rest.getForObject("http://AuditAPI/accessLogs/getAuditUserAccessLogs", String.class);
+		return rest.getForObject(AuditUserURL, String.class);
 	}
 
 	public String getSearchLog() {
-		return rest.getForObject("http://AuditAPI/searchLogs/getSearchLogs", String.class);
+		return rest.getForObject(AuditSearchURL, String.class);
 	}
 
 	public void sendAuditUserAccessLogs(String username, long id) {
