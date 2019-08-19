2ad9cf045c25a69bc89163be2abccaac09d90d0f
lushilling <lushilling@hotmail.co.uk>
2019-08-19 12:15:05 +0100

    annotations

--- src/test/java/com/bae/entity/AuditSearchLogTest.java
+++ src/test/java/com/bae/entity/AuditSearchLogTest.java
@@ -1,5 +1,11 @@
 package com.bae.entity;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AuditSearchLogTest {
 
 }
