7355235b0827e901f41349598471c49bf2d49b26
lushilling <lushilling@hotmail.co.uk>
2019-08-19 12:43:07 +0100

    audit request log tests

--- src/test/java/com/bae/entity/AuditRequestLogTests.java
+++ src/test/java/com/bae/entity/AuditRequestLogTests.java
@@ -1,5 +1,8 @@
 package com.bae.entity;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,4 +11,38 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AuditRequestLogTests {
 
+	AuditRequestLog account = new AuditRequestLog("name", 1L, null);
+
+	@Test
+	public void getUsernameTest() {
+		assertEquals("name", account.getUsername());
+	}
+
+	@Test
+	public void setUsernameTest() {
+		account.setUsername("new name");
+		assertEquals("new name", account.getUsername());
+	}
+
+	@Test
+	public void getIdTest() {
+		assertEquals(1L, account.getId());
+	}
+
+	@Test
+	public void setIdTest() {
+		account.setId(2L);
+		assertEquals(2L, account.getId());
+	}
+
+	@Test
+	public void getTimeStampTest() {
+		assertEquals(null, account.getTimeStamp());
+	}
+
+	@Test
+	public void setTimeStampTest() {
+		account.setTimeStamp(null);
+		assertEquals(null, account.getTimeStamp());
+	}
 }
