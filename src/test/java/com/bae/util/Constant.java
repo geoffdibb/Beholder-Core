75daaae27dd1b3a5be7e6e0b52c649bb741f6ff5
lushilling <lushilling@hotmail.co.uk>
2019-08-20 09:24:58 +0100

    refacotring

--- src/test/java/com/bae/util/Constant.java
+++ src/test/java/com/bae/util/Constant.java
@@ -9,8 +9,9 @@ import com.bae.entity.AuditRequestLog;
 
 public class Constant {
 
-	public static final AuditRequestLog MOCK_USER_OBJECT = new AuditRequestLog("user1", 1);
-	public static final AuditRequestLog MOCK_USER_OBJECT2 = new AuditRequestLog("user2", 2);
+	public static final AuditRequestLog MOCK_USER_OBJECT = new AuditRequestLog("audit", 1);
+	public static final AuditRequestLog MOCK_USER_OBJECT2 = new AuditRequestLog("audit", 2);
 	public static final Optional<AuditRequestLog> MOCK_USER_OBJECT3 = Optional.of(MOCK_USER_OBJECT);
 	public static final ResponseEntity<String> MOCK_USER_OBJECT4 = new ResponseEntity<String>("search", HttpStatus.OK);
+
 }
