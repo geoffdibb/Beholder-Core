eb6f7c1f70fd47f6d3edac3b7f01f174da562741
lushilling <lushilling@hotmail.co.uk>
2019-08-20 16:30:56 +0100

    fixed error

--- src/main/java/com/bae/controller/CoreController.java
+++ src/main/java/com/bae/controller/CoreController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bae.service.CoreService;
 
 @RestController
-@RequestMapping("${path.controller=/beholder}")
+@RequestMapping("${path.controller}")
 public class CoreController {
 
 	private CoreService service;
