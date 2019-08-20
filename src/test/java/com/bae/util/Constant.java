67f746fd11adecc4017745cfe9715038e1f43e3f
lushilling <lushilling@hotmail.co.uk>
2019-08-20 09:22:00 +0100

    setting up test constants

--- src/test/java/com/bae/util/Constant.java
+++ src/test/java/com/bae/util/Constant.java
@@ -1,5 +1,16 @@
 package com.bae.util;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.bae.entity.AuditRequestLog;
+
 public class Constant {
 
+	public static final AuditRequestLog MOCK_USER_OBJECT = new AuditRequestLog("user1", 1);
+	public static final AuditRequestLog MOCK_USER_OBJECT2 = new AuditRequestLog("user2", 2);
+	public static final Optional<AuditRequestLog> MOCK_USER_OBJECT3 = Optional.of(MOCK_USER_OBJECT);
+	public static final ResponseEntity<String> MOCK_USER_OBJECT4 = new ResponseEntity<String>("search", HttpStatus.OK);
 }
