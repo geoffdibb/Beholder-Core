62d119da0509880faa411d005a510367d7ad23f2
patelkarishma10 <patelkarishma10@gmail.com>
2019-08-20 16:44:49 +0100

    removed unused import

--- src/main/java/com/bae/controller/CoreController.java
+++ src/main/java/com/bae/controller/CoreController.java
@@ -1,7 +1,6 @@
 package com.bae.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
